0a0eea1de2a1c7fb7e6c49e1565f2ecefa507e38
Luke Ahn <ahnnuga@gmail.com>
2017-11-11 15:50:07 -0500

    home page created on a new branch

--- ollie_mobile/App.js
+++ ollie_mobile/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View, Text} from 'react-native';
 
-import MapView from 'react-native-maps';
-import MyMap from './MyMap';
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
 
-export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.map}>
-          <MyMap/>
+
+        <View style={styles.questionContainer}>
+          <Text style={styles.bigblue}> Do you believe in Self-Driving Cars?</Text>
         </View>
-        <View style={{flex: 3}}>
-          <Text>Other Components to go here</Text>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Yes"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="No"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Maybe"
+            color="#555555"
+          />
         </View>
       </View>
     );
@@ -21,9 +40,21 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   justifyContent: 'center',
   },
-});
+  buttonContainer: {
+    alignItems: 'center',
+    margin: 20,
+    // width: '100%'
+  },
+  questionContainer: {
+   justifyContent: 'center'
+  },
+  bigblue: {
+    textAlign: 'center',
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 20,
+  }
+})
