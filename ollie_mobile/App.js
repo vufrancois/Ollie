7a0d608df72f5c131a0c9ad2dbe84155b3cd29bd
Luke Ahn <ahnnuga@gmail.com>
2017-11-14 00:21:39 -0500

    tack-navigation implemented

--- ollie_mobile/App.js
+++ ollie_mobile/App.js
@@ -1,60 +1,85 @@
-import React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View, Text} from 'react-native';
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import { View, Text, Video, WebView, Button,TouchableWithoutFeedback, StyleSheet } from 'react-native';
 
-export default class ButtonBasics extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
-  }
+const HomeScreen = ({ navigation }) => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    
+    <Text>Do you believe in Self-Driving Cars?</Text>
 
-  render() {
-    return (
-      <View style={styles.container}>
-
-        <View style={styles.questionContainer}>
-          <Text style={styles.bigblue}> Do you believe in Self-Driving Cars?</Text>
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Yes"
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="No"
-            color="#841584"
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Maybe"
-            color="#555555"
-          />
-        </View>
+    <TouchableWithoutFeedback onPress={() => navigation.navigate('Content1')}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Yes!</Text>
       </View>
-    );
-  }
-}
+    </TouchableWithoutFeedback>
+
+    <TouchableWithoutFeedback onPress={() => navigation.navigate('Content1')}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>No!</Text>
+      </View>
+    </TouchableWithoutFeedback>
+
+    <TouchableWithoutFeedback onPress={() => navigation.navigate('Content1')}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Maybe</Text>
+      </View>
+    </TouchableWithoutFeedback>
+
+  </View>
+);
+
+
 
 const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   justifyContent: 'center',
-  },
-  buttonContainer: {
+  button: {
+    marginBottom: 30,
+    width: 260,
     alignItems: 'center',
-    margin: 20,
-    // width: '100%'
+    backgroundColor: '#2196F3'
   },
-  questionContainer: {
-   justifyContent: 'center'
-  },
-  bigblue: {
-    textAlign: 'center',
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 20,
+  buttonText: {
+    padding: 20,
+    color: 'white'
   }
 })
+
+
+const Content1 = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <WebView
+        style={{flex:1}}
+        javaScriptEnabled={true}
+        source={{uri: 'https://www.youtube.com/embed/ZZ5LpwO-An4?rel=0&autoplay=0&showinfo=0&controls=0'}}
+    /> 
+  </View>
+);
+
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      headerTitle: 'Home',
+      headerStyle: { marginTop: 24 },
+    },
+  },
+
+  Content1: {
+    screen: Content1,
+    navigationOptions: {
+      headerTitle: 'Content1',
+      headerStyle: { marginTop: 24 },
+    },
+  },
+
+  // Content2: {
+  //   screen: Content2,
+  //   navigationOptions: {
+  //     headerTitle: 'Content2',
+  //     headerStyle: { marginTop: 24 },
+  //   },
+  // },
+
+});
+
+export default RootNavigator;
+
